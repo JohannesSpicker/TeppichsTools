2354da5815c72158e49cdb412bb5d5d1c04cb0b5
teppich <johannesspicker@me.com>
2021-04-20 21:12:47 +0200

    - collector now gets thing in OnEnable

--- TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/Collector.cs
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/Collector.cs
@@ -6,9 +6,9 @@ public class Collector<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static List<T> collection = new List<T>();
 
-    //use onValidate or something different to fill field
-    [SerializeField] private T thing;
+    private T thing;
 
-    private void OnEnable()  => collection.Add(thing);
+    private void OnEnable() => collection.Add(thing ??= GetComponent<T>());
+
     private void OnDisable() => collection.Remove(thing);
 }
