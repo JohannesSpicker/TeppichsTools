2ad037e43403352131a4f0222873bad2b96ca159
teppich <johannesspicker@me.com>
2021-05-09 16:43:23 +0200

    added basic blackboard

---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Data/Blackboard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeppichsTools.Data
+{
+    [Serializable]
+    public class Blackboard
+    {
+        private Dictionary<Type, Dictionary<string, object>> library =
+            new Dictionary<Type, Dictionary<string, object>>();
+
+        public void Clear() => library.Clear();
+
+        public void Write<T>(string id, T value)
+        {
+            if (!library.ContainsKey(typeof(T)))
+                library.Add(typeof(T), new Dictionary<string, object>());
+
+            library[typeof(T)].Add(id, value);
+        }
+
+        public T Read<T>(string id)
+        {
+            try
+            {
+                return (T) library[typeof(T)][id];
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ TeppichsTools/Assets/TeppichsTools/Tests/Editor/BlackboardTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using TeppichsTools.Data;
+
+namespace TeppichsTools.Editor.Tests
+{
+    public class BlackboardTests
+    {
+        [Test]
+        public void ShouldWriteAndReadValue()
+        {
+            Setup(out Blackboard blackboard);
+
+            string id    = "TestName";
+            string value = "TestValue";
+
+            blackboard.Write(id, value);
+
+            Assert.AreEqual(value, blackboard.Read<string>(id));
+        }
+
+        [Test]
+        public void ShouldWriteAndReadValuesOfDifferentTypes()
+        {
+            Setup(out Blackboard blackboard);
+
+            string stringId    = "StringTestId";
+            string stringValue = "StringTestValue";
+
+            string intId    = "IntTestName";
+            int    intValue = 42;
+
+            blackboard.Write(stringId, stringValue);
+            blackboard.Write(intId,    intValue);
+
+            Assert.AreEqual((stringValue, intValue), (blackboard.Read<string>(stringId), blackboard.Read<int>(intId)));
+        }
+
+        [Test]
+        public void ShouldReturnNullOnWrongID()
+        {
+            Setup(out Blackboard blackboard);
+            
+            Assert.IsNull(blackboard.Read<string>("id"));
+        }
+
+        private void Setup(out Blackboard blackboard) => blackboard = new Blackboard();
+    }
+}
