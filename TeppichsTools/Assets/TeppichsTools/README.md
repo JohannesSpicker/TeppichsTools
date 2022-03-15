4512adcfa70a5ec7215319a481d6f54f230ac583
teppich <johannesspicker@me.com>
2022-03-15 14:30:46 +0100

    Added vector extensions

---
+++ TeppichsTools/Assets/TeppichsTools/README.md
@@ -0,0 +1,3 @@
+Ideas:
+- Audio player (i.e. for easy random pitch)
+- vector math (i.e. is distance < range?)
---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Math/VectorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TeppichsTools.Runtime.Math
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 DropX(this Vector2 vector) => new Vector2(0f,       vector.y);
+        public static Vector2 DropY(this Vector2 vector) => new Vector2(vector.x, 0f);
+
+        public static Vector3 DropX(this Vector3 vector) => new Vector3(0f,       vector.y, vector.z);
+        public static Vector3 DropY(this Vector3 vector) => new Vector3(vector.x, 0f,       vector.z);
+        public static Vector3 DropZ(this Vector3 vector) => new Vector3(vector.x, vector.y, 0f);
+    }
+}
