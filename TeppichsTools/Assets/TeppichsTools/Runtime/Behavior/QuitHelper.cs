b91eb57c96da1bc7f6c7079b06312aa9ac1fb923
teppich <johannesspicker@me.com>
2022-09-22 19:43:44 +0200

    Added QuitHelper

---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/QuitHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace TeppichsTools.Behavior
+{
+    public static class QuitHelper
+    {
+#if UNITY_WEBPLAYER
+     public static string webplayerQuitURL = "about:blank";
+#endif
+        public static void Quit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+            Application.OpenURL(webplayerQuitURL);
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
