1f6b972c7d6353fc2a5b9ba924aecb02ac27106d
teppich <johannesspicker@me.com>
2022-12-19 17:49:06 +0100

    Added Linq extension product

---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Math/LinqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TeppichsTools.Math
+{
+    public static class LinqExtensions
+    {
+        public static float  Product(this IEnumerable<float>  source) => source.Aggregate(1f, (acc, val) => acc * val);
+        public static int    Product(this IEnumerable<int>    source) => source.Aggregate(1,  (acc, val) => acc * val);
+        public static double Product(this IEnumerable<double> source) => source.Aggregate(1d, (acc, val) => acc * val);
+        public static long   Product(this IEnumerable<long>   source) => source.Aggregate(1L, (acc, val) => acc * val);
+
+        public static decimal Product(this IEnumerable<decimal> source) =>
+            source.Aggregate(decimal.One, (acc, val) => acc * val);
+    }
+}
