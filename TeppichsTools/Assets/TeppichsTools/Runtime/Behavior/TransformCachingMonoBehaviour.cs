beabe19cea0ecf53af3f72d46f92e96d070ae3e0
teppich <johannesspicker@me.com>
2022-12-19 21:43:19 +0100

    Added TransformCachingMonoBehaviour

---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/TransformCachingMonoBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TeppichsTools.Behavior
+{
+    // ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
+    public class TransformCachingMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField] public Transform myTransform;
+
+        public virtual void OnValidate() => myTransform = transform;
+    }
+}
