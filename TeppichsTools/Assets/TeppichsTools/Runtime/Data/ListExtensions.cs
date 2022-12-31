11a058450e6e6bbbe3e521982a433fca85dc6539
teppich <johannesspicker@me.com>
2022-12-31 02:35:19 +0100

    Added shuffle list extension and threadSafe random

---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Data/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TeppichsTools.Math.Randomness;
+
+namespace TeppichsTools.Data
+{
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            for (int i = list.Count - 1; 0 < i; --i)
+            {
+                int swap = ThreadSafeRandom.ThisThreadsRandom.Next(i + 1);
+                (list[i], list[swap]) = (list[swap], list[i]);
+            }
+        }
+    }
+}
---
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Math/Randomness/ThreadSafeRandom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace TeppichsTools.Math.Randomness
+{
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic] private static Random local;
+
+        public static Random ThisThreadsRandom => local ??=
+            new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId));
+    }
+}
