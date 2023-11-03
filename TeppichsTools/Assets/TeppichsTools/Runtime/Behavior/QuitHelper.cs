d59a82b35f820f79a4ee2640c04a1049c90fe29f
teppich <johannesspicker@me.com>
2023-11-03 19:44:18 +0100

    fixed pragmas

--- TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/QuitHelper.cs
+++ TeppichsTools/Assets/TeppichsTools/Runtime/Behavior/QuitHelper.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
-#if !UNITY_EDITOR
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+#else
 using UnityEngine;
 #endif
 
--- TeppichsTools/Assets/TeppichsTools/Samples/Aggregates/Thing.cs
+++ TeppichsTools/Assets/TeppichsTools/Samples/Aggregates/Thing.cs
@@ -1,3 +1,13 @@
+using System;
+using TeppichsTools.Data;
 using UnityEngine;
 
-public class Thing : MonoBehaviour { }
+[Serializable]
+public class Thing : MonoBehaviour
+{
+
+    [SerializeField] public someDictionary bla = new someDictionary();
+
+}
+
+public class someDictionary : UnitySerializedDictionary<int, int>{}
